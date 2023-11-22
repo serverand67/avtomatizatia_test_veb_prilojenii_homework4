b2cbebf1d359bb031783adec5baa2251b03d7ec9
Veronika040182 <veronika040182@hotmail.com>
2023-11-22 22:01:16 +0300

    Initial commit

---
+++ seminar1/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture()
+def good_word():
+    return 'кашалот'
+
+
+@pytest.fixture()
+def bad_word():
+    return "кошалот"
---
+++ seminar1/task1.py
@@ -0,0 +1,25 @@
+# С использованием фреймворка pytest написать тест операции checkText SOAP API https://speller.yandex.net/services/spellservice?WSDL
+#
+# Тест должен использовать DDT и проверять наличие определенного
+# верного слова в списке предложенных исправлений к определенному неверному слову.
+#
+# Слова должны быть заданы через фикстуры в conftest.py,
+# адрес wsdl должен быть вынесен в config.yaml.
+#
+# Методы работы с SOAP должны быть вынесены в отдельную библиотеку.
+
+from zeep import Client, Settings
+import yaml
+
+with open(r'C:\Users\NiKa\PycharmProjects\avtomatizatia_test_veb_prilojenii\seminar1\config.yaml') as f:
+    data = yaml.safe_load(f)
+
+# print(data)
+
+def check_word(word):
+    url = data['url']
+    settings = Settings(strict=False)
+    client = Client(wsdl=url, settings=settings)
+    # print(client.service.checkText('кошкав')[0]['s'])
+    return client.service.checkText(word)[0]['s']
+
